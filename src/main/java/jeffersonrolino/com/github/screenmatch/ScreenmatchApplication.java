af6c313bf14a23beb09098803bdacbbb76957786
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-10 19:15:03 -0300

    Java Spring - Curso 01 - Capítulo 01 - Aula 06: Consumindo dados de séries

--- src/main/java/jeffersonrolino/com/github/screenmatch/ScreenmatchApplication.java
+++ src/main/java/jeffersonrolino/com/github/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,18 @@
 package jeffersonrolino.com.github.screenmatch;
 
-import jeffersonrolino.com.github.screenmatch.service.ConsumoApi;
+import jeffersonrolino.com.github.screenmatch.service.QueryApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
+	@Value("${apikey}")
+	private String apikey;
+	private String apiAddress = "https://www.omdbapi.com/";
+	private String searchedMovieOrSeries = "game+of+thrones";
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
@@ -14,6 +20,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello from Spring...");
+		String queryString = apiAddress + "?t=" + searchedMovieOrSeries + "&Season=1" + "&apikey=" + apikey;
+		QueryApi queryApi = new QueryApi();
+		String json = queryApi.retrieveData(queryString);
+		System.out.println(json);
 	}
 }
---
+++ src/main/java/jeffersonrolino/com/github/screenmatch/service/QueryApi.java
@@ -0,0 +1,27 @@
+package jeffersonrolino.com.github.screenmatch.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class QueryApi {
+    public String retrieveData(String endereco) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        String json = response.body();
+        return json;
+    }
+
+}
