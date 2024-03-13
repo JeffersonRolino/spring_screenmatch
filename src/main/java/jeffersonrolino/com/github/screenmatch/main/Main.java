f0a26de0aad4685cdcc29bb67eb3a5ab8c53d8b7
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-12 21:26:34 -0300

    Java Spring - Curso 01 - Capítulo 03 - Aula 02: Encadeando operações com streams

--- src/main/java/jeffersonrolino/com/github/screenmatch/main/Main.java
+++ src/main/java/jeffersonrolino/com/github/screenmatch/main/Main.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -23,26 +24,39 @@ public class Main {
     }
 
     public void showMenu(){
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Digite o nome da série para busca: ");
+//        String query = scanner.nextLine().toLowerCase().replace(' ', '+');
+//
+//        String queryString = API_ADDRESS + query + "&apikey=" + apikey;
+//        QueryApi queryApi = new QueryApi();
+//        String json = queryApi.retrieveData(queryString);
+//
+//        DataConverter dataConverter = new DataConverter();
+//        SeriesData seriesData = dataConverter.convertData(json, SeriesData.class);
+//
+//		List<SeasonData> seasons = new ArrayList<>();
+//
+//		for (int i = 0; i < seriesData.totalSeasons(); i++) {
+//			String jsonSeasons = queryApi.retrieveData(API_ADDRESS + query + "&Season=" + String.valueOf(i + 1) + "&apikey=" + apikey);
+//			SeasonData seasonData = dataConverter.convertData(jsonSeasons, SeasonData.class);
+//			seasons.add(seasonData);
+//		}
+//
+//		seasons.forEach(System.out::println);
 
-        System.out.println("Digite o nome da série para busca: ");
-        String query = scanner.nextLine().toLowerCase().replace(' ', '+');
+        // Printing all Episodes for all seasons
+//        seasons.forEach(season -> season.episodes().forEach(episode -> System.out.println(episode.title())));
 
-        String queryString = API_ADDRESS + query + "&apikey=" + apikey;
-        QueryApi queryApi = new QueryApi();
-        String json = queryApi.retrieveData(queryString);
+        // Working with streams
+        List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Rodrigo", "Nico");
 
-        DataConverter dataConverter = new DataConverter();
-        SeriesData seriesData = dataConverter.convertData(json, SeriesData.class);
-
-		List<SeasonData> seasonDataList = new ArrayList<>();
-
-		for (int i = 0; i < seriesData.totalSeasons(); i++) {
-			String jsonSeasons = queryApi.retrieveData(API_ADDRESS + query + "&Season=" + String.valueOf(i + 1) + "&apikey=" + apikey);
-			SeasonData seasonData = dataConverter.convertData(jsonSeasons, SeasonData.class);
-			seasonDataList.add(seasonData);
-		}
-
-		seasonDataList.forEach(System.out::println);
+        nomes.stream()
+                .sorted()
+                .limit(3)
+                .filter(name -> name.startsWith("N"))
+                .map(name -> name.toUpperCase())
+                .forEach(System.out::println);
     }
 }
