68f17b93f2f698e2ee171f3982932b3f7b4ada53
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-18 08:33:05 -0300

    Java Spring - Curso 02 - Capítulo 01 - Aula 04: Adicionando mais propriedades à série

--- src/main/java/jeffersonrolino/com/github/screenmatch/model/SeriesData.java
+++ src/main/java/jeffersonrolino/com/github/screenmatch/model/SeriesData.java
@@ -6,5 +6,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SeriesData(@JsonAlias("Title") String title,
                          @JsonAlias("totalSeasons") Integer totalSeasons,
-                         @JsonAlias("imdbRating") String review){
+                         @JsonAlias("imdbRating") String review,
+                         @JsonAlias("Genre") String genre,
+                         @JsonAlias("Actors") String actors,
+                         @JsonAlias("Poster") String poster,
+                         @JsonAlias("Plot") String synopses
+                         ){
 }
